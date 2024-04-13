3b7e9e678cde07f06cdac3225e5c8ef2d71230f5
Hector Aguero <hector.aguero.brisso@gmail.com>
2024-04-13 00:22:59 -0400

    Refactor school_color_hook.dart to improve color mapping logic

--- lib/features/schools/school_color_hook.dart
+++ lib/features/schools/school_color_hook.dart
@@ -17,70 +17,45 @@ class ColorHook extends MappingHook {
     return value;
   }
 
-  Color _getColor(Object color) => switch (color.toString().toLowerCase()) {
-        ('red' || 'vermelho' || 'rojo' || 'あか' || '赤' || 'レッド') => Colors.red,
-        ('pink' || 'rosa' || 'ピンク' || '桃色') => Colors.pink,
-        ('purple' || 'roxo' || 'morado' || 'むらさき' || '紫') => Colors.purple,
-        ('deep purple' ||
-              'roxo escuro' ||
-              'púrpura profundo' ||
-              'こいむらさき' ||
-              '濃紫') =>
-          Colors.deepPurple,
-        ('indigo' || 'índigo' || 'índigo' || 'インディゴ' || '藍色') => Colors.indigo,
-        ('blue' ||
-              'navy blue' ||
-              'azul' ||
-              'azul' ||
-              'azul marino' ||
-              'あお' ||
-              'ネイビーブルー' ||
-              'ブルー' ||
-              '青') =>
-          Colors.blue,
-        ('light blue' || 'azul claro' || 'azul claro' || 'あおいろ' || '浅藍') =>
-          Colors.lightBlue,
-        ('cyan' ||
-              'peacock blue' ||
-              'azul-pavo' ||
-              'ciano' ||
-              'cian' ||
-              'みずいろ' ||
-              '水色' ||
-              'ピーコック・ブルー') =>
-          Colors.cyan,
-        ('teal' || 'verde azulado' || 'verde azulado' || 'アクア' || '青緑') =>
-          Colors.teal,
-        ('green' || 'verde' || 'verde' || 'みどり' || '緑' || 'グリーン') =>
-          Colors.green,
-        ('light green' || 'verde claro' || 'verde claro' || 'うすみどり' || '浅緑') =>
-          Colors.lightGreen,
-        ('lime' || 'lima' || 'lima' || 'ライム' || 'ライム色') => Colors.lime,
-        ('yellow' || 'amarelo' || 'amarillo' || 'きいろ' || '黄色' || 'イエロー') =>
-          Colors.yellow,
-        ('amber' || 'âmbar' || 'ámbar' || 'こはくいろ' || '琥珀色') => Colors.amber,
-        ('gold' || 'oro' || 'ouro' || 'ゴールド') => Colors.amber,
-        ('orange' || 'laranja' || 'naranja' || 'オレンジ' || '橙色') => Colors.orange,
-        ('deep orange' ||
-              'laranja escuro' ||
-              'naranja oscuro' ||
-              'だいだいいろ' ||
-              '橙色') =>
-          Colors.deepOrange,
-        ('brown' || 'marrom' || 'marrón' || 'ちゃいろ' || '茶色') => Colors.brown,
-        ('grey' || 'cinza' || 'gris' || 'はいいろ' || '灰色') => Colors.grey,
-        ('blue grey' || 'azul cinza' || 'azul grisáceo' || 'あおはいいろ' || '藍灰色') =>
-          Colors.blueGrey,
-        ('black' || 'preto' || 'negro' || 'くろ' || 'ブラック' || '黒') =>
-          Colors.black,
-        ('white' || 'branco' || 'blanco' || 'しろ' || '白' || 'ホワイト') =>
-          Colors.white,
-        // Add more colors as needed
-        (_) => _defaultColor(color.toString()) // default case
-      };
-}
+  Color _getColor(Object color) {
+    return switch (color.toString().replaceAll('-', ' ').toLowerCase()) {
+      'anil' => Colors.indigo,
+      'vermelho' => Colors.red,
+      'rosa' => Colors.pink,
+      'roxo' || 'morado' => Colors.purple,
+      'roxo escuro' || 'púrpura profundo' => Colors.deepPurple,
+      'indigo' || 'índigo' => Colors.indigo,
+      'azul' => Colors.blue,
+      'azul claro' => Colors.lightBlue,
+      'azul pavo' || 'ciano' => Colors.cyan,
+      'verde azulado' => Colors.teal,
+      'verde' => Colors.green,
+      'verde claro' => Colors.lightGreen,
+      'lima' => Colors.lime,
+      'amarelo' => Colors.yellow,
+      'amber' || 'âmbar' || 'ámbar' || 'amarelo ouro' => Colors.amber,
+      'ouro' || 'dourado' => Colors.amber,
+      'laranja' => Colors.orange,
+      'laranja escuro' => Colors.deepOrange,
+      'marrom' => Colors.brown,
+      'cinza' => Colors.grey,
+      'azul grisáceo' => Colors.blueGrey,
+      'preto' || 'negro' => Colors.black,
+      'branco' => Colors.white,
+      'azul marinho' => Colors.blue.shade900,
+      'azul pavão' => Colors.cyan,
+      'azul royal' => Colors.blue.shade800,
+      'azul turquesa' => Colors.cyan.shade400,
+      'violeta' => Colors.purple.shade800,
+      'prata' => Colors.grey.shade400,
+      'encarnado' => Colors.red.shade700,
+      'azul escuro' => Colors.blue.shade800,
+      _ => _defaultColor(color.toString())
+    };
+  }
 
-Color _defaultColor(String color) {
-  logColorParse.info('Color not parsed $color');
-  return Colors.white;
+  Color _defaultColor(String color) {
+    logColorParse.info('Color not parsed $color');
+    return Colors.white;
+  }
 }
